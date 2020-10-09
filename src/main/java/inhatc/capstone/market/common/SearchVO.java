a685e9fbf545b62ffa9cb9fa625b5ba44e967eab
Routine <jinkim0427@naver.com>
2020-10-10 04:56:37 +0900

    매장 찾기 기능 등록
    그 외 변동 사항
    - AbstractDAO 위치 변경
    - DB에 대한 내용 변경

---
+++ src/main/java/inhatc/capstone/market/common/SearchVO.java
@@ -0,0 +1,21 @@
+package inhatc.capstone.market.common;
+
+public class SearchVO {
+	private String searchKeyword = "";
+	private int searchKey;
+	public String getSearchKeyword() {
+		return searchKeyword;
+	}
+
+	public void setSearchKeyword(String searchKeyword) {
+		this.searchKeyword = searchKeyword;
+	}
+
+	public int getSearchKey() {
+		return searchKey;
+	}
+
+	public void setSearchKey(int searchKey) {
+		this.searchKey = searchKey;
+	}
+}
