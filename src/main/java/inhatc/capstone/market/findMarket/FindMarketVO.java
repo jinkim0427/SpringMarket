7c4742eb69ffcc2b9e094747a918fc396c1bd0d2
DinuKim <jinkim0427@naver.com>
2020-10-11 19:43:44 +0900

    findMarket 수정
    - DB 변경에 의한 VO변경 및 JSP 변경

--- src/main/java/inhatc/capstone/market/findMarket/FindMarketVO.java
+++ src/main/java/inhatc/capstone/market/findMarket/FindMarketVO.java
@@ -6,9 +6,7 @@ public class FindMarketVO {
 	private String mk_name;
 	private String mk_address;
 	private String mk_tel;
-	private String mk_open_time;
-	private String mk_close_time;
-	private String mk_closed;
+	private String mk_state;
 	private String mk_delivery;
 	private String mk_intro;
 	
@@ -42,23 +40,11 @@ public class FindMarketVO {
 	public void setMk_tel(String mk_tel) {
 		this.mk_tel = mk_tel;
 	}
-	public String getMk_open_time() {
-		return mk_open_time;
+	public String getMk_state() {
+		return mk_state;
 	}
-	public void setMk_open_time(String mk_open_time) {
-		this.mk_open_time = mk_open_time;
-	}
-	public String getMk_close_time() {
-		return mk_close_time;
-	}
-	public void setMk_close_time(String mk_close_time) {
-		this.mk_close_time = mk_close_time;
-	}
-	public String getMk_closed() {
-		return mk_closed;
-	}
-	public void setMk_closed(String mk_closed) {
-		this.mk_closed = mk_closed;
+	public void setMk_state(String mk_state) {
+		this.mk_state = mk_state;
 	}
 	public String getMk_delivery() {
 		return mk_delivery;
